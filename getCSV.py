23f2987aa905c6a8836ac52355bad79879d20107
karnkung2 <thanapol.phu@gmail.com>
2017-07-02 20:04:23 +0700

    update

--- getCSV.py
+++ getCSV.py
@@ -2,12 +2,11 @@ import pandas as pd
 import numpy as np
 
 ratingsList = [i.strip().split(",") for i in open('ratings.csv', 'r').readlines()]
-# users_list = [i.strip().split(",") for i in open('users.dat', 'r').readlines()]
 moviesList = [i.strip().split(",") for i in open('movies.csv', 'r', encoding="utf8").readlines()]
 
 ratingsDataFrame = pd.DataFrame(ratingsList, columns = ['UserID', 'MovieID', 'Rating', 'Timestamp'], dtype = int)
 ratingsDataFrame.drop(ratingsDataFrame.index[:1], inplace=True)
-# print(ratings_df.head())
+
 
 for index, val in enumerate(moviesList):
     if len(val)>3:
@@ -20,3 +19,8 @@ moviesDataFrame.drop(moviesDataFrame.index[:1], inplace=True)
 moviesDataFrame['MovieID'] = moviesDataFrame['MovieID'].apply(pd.to_numeric)
 print(moviesDataFrame.head())
 
+joinDataFrame = ratingsDataFrame.pivot(index = 'UserID', columns ='MovieID', values = 'Rating').fillna(0)
+joinMatrix = joinDataFrame.as_matrix()
+print(joinMatrix)
+print(joinMatrix.shape)
+# ratingMean = np.mean(str(joinMatrix), axis = 1)
